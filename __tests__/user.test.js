c42e3ff06ede130dd88bc1e0e4d82272fb9b178e
Jmart5564 <Jmart5564@gmail.com>
2022-08-16 12:35:55 -0700

    passing test for new user

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -2,20 +2,20 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const testUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'testuser@email.com',
+  password: 'password',
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
   it('should create a new user', async () => {
-    const newUser = {
-      firstName: 'New',
-      lastName: 'User',
-      email: 'newuser@email.com',
-      password: 'password',
-    };
-    const res = await request(app).post('/api/v1/users').send(newUser);
-    const { firstName, lastName, email } = newUser;
+    const res = await request(app).post('/api/v1/users').send(testUser);
+    const { firstName, lastName, email } = testUser;
 
     expect(res.status).toBe(200);
     expect(res.body).toEqual({
